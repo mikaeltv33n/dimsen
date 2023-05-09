c10034ebad849c0aff74d92575134d034c678695
mikaeltv33n <mikaeltv33n>
2023-05-09 08:55:02 +0200

    code project

---
+++ script.js
@@ -0,0 +1,11 @@
+
+// vi invokere onclick eventet fra html med funktionen changeBackground
+function changeBackground() {
+    document.querySelector('.contentBox').style.backgroundColor = 'blue' 
+   
+}     
+function changeBackground2() {
+    document.querySelector('.contentBox').style.backgroundColor = 'green' 
+   
+}
+
